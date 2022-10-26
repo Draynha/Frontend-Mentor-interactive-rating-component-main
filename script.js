c61826aa96a75f032af9d6987d6f9a0b49e7c93b
Draynha <17475326+Draynha@users.noreply.github.com>
2022-10-26 20:35:29 +0200

    final 1.0

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 "use strict";
 const btn = document.querySelectorAll(".btn");
 const ratingValue = document.querySelector(".rating-value");
-for (let i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", function () {
-        const value = btn[i].childNodes[0].textContent;
-        ratingValue.innerHTML = value;
+const btnsubmit = document.querySelector(".btn-submit-rating");
+const ratingDiv = document.querySelector(".rating-div");
+const resultDiv = document.querySelector(".result-div");
+const warnElement = document.querySelector(".warn");
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        btn.forEach((styleRemove => {
+            styleRemove.removeAttribute('style');
+        }));
+        ratingValue.innerHTML = button.childNodes[0].textContent;
+        const selectedBtn = button;
+        selectedBtn.style.background = "hsl(216, 12%, 54%)";
     });
-}
+});
+btnsubmit.addEventListener('click', () => {
+    if (ratingValue.textContent !== '0') {
+        ratingDiv.style.display = "none";
+        resultDiv.style.display = "block";
+    }
+    else {
+        warnElement.style.display = "block";
+    }
+});
